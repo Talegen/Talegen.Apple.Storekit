14f796f95d2d8909ffec51b79edc569096f2602d
Rob Kennedy <rob@talegen.com>
2025-02-16 00:21:27 -0500

    adding a test console app to test the API

---
+++ test/TestAppKit/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace TestAppKit
+{
+    using Talegen.Apple.Storekit.Models.Settings;
+    using Talegen.Apple.Storekit.Client;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.DependencyInjection;
+    using Talegen.Apple.Storekit.Extensions;
+    using static System.Net.Mime.MediaTypeNames;
+
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Apple Kit Tester!");
+            var builder = new HostBuilder()
+               .ConfigureServices((hostContext, services) =>
+               {
+                   services.AddHttpClient();
+                   services.AddAppleApiClient(config =>
+                   {
+                       config.IssuerId = "<Your IssuerId Guid>";
+                       config.KeyId = "<Your Key Id>";
+                       config.BundleId = "<Your Bundle Id>";
+                       config.Environment = Talegen.Apple.Storekit.Models.EnvironmentType.Production;
+                       config.PrivateKey = "-----Your Private Key-----";
+
+                   });
+                   services.AddTransient<TestApp>();
+               }).UseConsoleLifetime();
+            var host = builder.Build();
+
+            using (var serviceScope = host.Services.CreateScope())
+            {
+                var services = serviceScope.ServiceProvider;
+
+                try
+                {
+                    string testTransactionId = "1000000826908824";
+                    var myService = services.GetRequiredService<TestApp>();
+                    await myService.Run(testTransactionId);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error Occured");
+                }
+            }
+        }
+    }
+}
---
+++ test/TestAppKit/TestApp.cs
@@ -0,0 +1,56 @@
+﻿namespace TestAppKit
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Logging;
+    using Talegen.Apple.Storekit.Client;
+    using Talegen.Apple.Storekit.Extensions;
+
+    internal class TestApp
+    {
+        private readonly ILogger logger;
+
+        private readonly IAppStoreServerApiClient client;
+
+        public TestApp(ILogger<TestApp> logger, IAppStoreServerApiClient client)
+        {
+            this.logger = logger;
+            this.client = client;
+        }
+
+        public async Task Run(string testTransactionId)
+        {
+            this.logger.LogInformation("Application {applicationEvent} at {dateTime}", "Started", DateTime.UtcNow);
+            try
+            {
+                var response = await this.client.GetTransactionAsync(testTransactionId);
+            }
+            catch (AppleApiException aex)
+            {
+                if (aex.HttpStatusCode == System.Net.HttpStatusCode.NotFound)
+                {
+                    this.logger.LogWarning("The specified resource was not found.");
+
+                    try
+                    {
+                        // try sandbox
+                        var response = await this.client.GetTransactionAsync(testTransactionId, environment: Talegen.Apple.Storekit.Models.EnvironmentType.Sandbox);
+
+                    }
+                    catch (AppleApiException aex2)
+                    {
+                        this.logger.LogError(aex2, "An error occured while processing your request.");
+                    }
+                }
+                else
+                {
+                    this.logger.LogError(aex, "An error occured while processing your request.");
+
+                }
+            }
+        }
+    }
+}
